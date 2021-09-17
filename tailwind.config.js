44a77e85bc95b9e9ddac8bf93d8c518590193d4d
3stbn <esteban93torres@gmail.com>
2021-09-17 12:38:11 -0500

    put back purge, add safelist of classes based on the labels colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
+const labelsClasses = [
+  "indigo",
+  "gray",
+  "green",
+  "blue",
+  "red",
+  "purple",
+];
+
 module.exports = {
-  purge: [],
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    //Because we made a dynamic class with the label we need to add those clases
+    // to the safe list so the purge does not remove that
+    safelist: [
+      ...labelsClasses.map((lbl) => `bg-${lbl}-500`),
+      ...labelsClasses.map((lbl) => `bg-${lbl}-200`),
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
